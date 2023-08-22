6ff73cc12abee3d0c5203074c1659c860c49d5d6
Buchstabenwurst <jo.okeleke.yahoo.de@gmail.com>
2023-08-22 03:15:27 +0200

    changes some stuff

--- assets/Convert  Mesh 128.bat
+++ assets/Convert  Mesh 128.bat
@@ -1,2 +1,2 @@
-for %%i in (%*) do python "C:\dev\ds\tools\obj2dl\obj2dl.py" --input %%i --output "C:\dev\ds\Portal-DS\data\%%~ni.bin" --texture "128" "128"
+for %%i in (%*) do python "C:\Users\Joshua\dev\ds\tools\obj2dl\obj2dl.py" --input %%i --output "C:\Users\Joshua\dev\ds\Portal-DS\data\%%~ni.bin" --texture "128" "128"
 pause
